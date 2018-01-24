31e094b54f0386d3a031f6c322597e8cb0b61cb8
cfsloth <Claudio@DESKTOP-3OC74SN>
2018-01-24 18:25:09 +0000

    Acess to database done. Next thing is the mapping of the webService

--- index.php
+++ index.php
@@ -1,16 +1,33 @@
 <?php
     class Database{
-            public  $servername = "localhost:81";
-            public  $username = "username";
-            public  $password = "password";
-            
-            function __construct() {
-                print "In BaseClass constructor\n";
-                if (!$link) {
-                    die('Could not connect: ' . mysql_error());
-                }
-                echo 'Connected successfully';
-                mysql_close($link);
+        
+        function __construct() {
+            //echo "Connecting to MySQL";
+            return mysqli_connect("localhost", "root", "","IM") 
+                    or die("Couldn't connect to Mysql");
+        }
+        
+        /**
+         * 
+         * @param object $dbhandle - return from the constructor
+         * @param string $query - SQL
+         * @return AssociativeArray results
+         */
+        function getData($dbhandle,$query){
+            $result = mysqli_query($dbhandle, $query);
+            if(empty($result)){
+                die("The query performance could not return any data");
             }
+            return mysqli_fetch_array($result);
+        }
+        
+        function setData($dbhandle,$query){
+            $result = mysqli($dbhandle,$query);
+            if(!$result){
+                die("The query performance could not change any data");
+            }
+            return 1;
+        }
+        
     }
-?>
+    
