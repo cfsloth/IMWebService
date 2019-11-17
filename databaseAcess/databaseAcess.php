7ecfd0a7f9c64f117a0e37c2788f2db155d02590
cfsloth@github.com <>
2019-11-17 23:09:52 +0000

    Update web-service to add a remote database. Next thing is to convert from a SOAP to a REST web-service

--- databaseAcess/databaseAcess.php
+++ databaseAcess/databaseAcess.php
@@ -1,5 +1,55 @@
 <?php
+    
+    /*
+     *drop table users;
+      drop table information_warning;
+      drop table usertype;
+
+    CREATE TABLE usertype(
+	id_type int PRIMARY KEY AUTO_INCREMENT,
+        type_name varchar(50),
+        description varchar(50)
+    );
+    CREATE TABLE users (
+        id_user int PRIMARY KEY AUTO_INCREMENT,
+        firstName varchar(50),
+        lastName varchar(50),
+        PASSWORD varchar(50),
+        usertype_id_type INT,
+        department varchar(50),
+        POSITION varchar(50),
+        email varchar(50),
+        deleted int,
+        CONSTRAINT fk_usertype_user
+        FOREIGN KEY (usertype_id_type) REFERENCES usertype(id_type)
+    )
+    
+    CREATE TABLE information_warning(
+	information_warning_id int PRIMARY KEY AUTO_INCREMENT,
+        user_sendind_id int,
+        user_receiving_id int,
+        description varchar(150),
+	severity int,
+        subject varchar(50),
+        CONSTRAINT fk_information_warning_users_sending
+        FOREIGN KEY (user_sendind_id)
+        REFERENCES users(id_user),
+	CONSTRAINT fk_information_warning_users_receiving
+        FOREIGN KEY (user_receiving_id)
+        REFERENCES users(id_user)
+    )
+     * 
+     * 
+     */
+
+
     class Database{
+        
+        static $remote_host = "remotemysql.com";
+        static $database_name = "QBLxoTXcdJ";
+        static $username = "QBLxoTXcdJ";
+        static $password = "1IIaQpert9";
+        
         /**
              * 
              * @return type dbhandle
@@ -9,9 +59,9 @@
             }
             
             function openConection(){
-                $con = mysqli_connect("localhost","root","") 
+                $con = mysqli_connect($remote_host,$username,$password) 
                         or die("Could not to mysqlDatabase");
-                mysqli_select_db($con,"IM");
+                mysqli_select_db($con,$databasename);
                 return $con;
             }
             
